912f74b127530f0953853f61a30435ea636d80cf
xemmel <lacour@gmail.com>
2014-07-13 16:52:41 +0200

    Added Person DB

---
+++ Small2.WebApp/Migrations/201407131448194_Init.cs
@@ -0,0 +1,27 @@
+namespace Small2.WebApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.People",
+                c => new
+                    {
+                        PersonId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Born = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.PersonId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.People");
+        }
+    }
+}
---
+++ Small2.WebApp/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Small2.WebApp.Models
+{
+  public class DataContext : DbContext
+  {
+    public DbSet<Person> Persons { get; set; }
+  }
+}
